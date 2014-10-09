2a5fa2a74931bfeb92810cb4ea3b37ec72c539b9
BlackSSI <smzy.black@outlook.com>
2014-10-09 22:49:21 +0900

    no message

---
+++ ListupEditFileFromWorkItem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListupEditFileFromWorkItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("接続先={0}, 作業項目の種類={1}, 作業項目ID={2}", args[0], args[1], args[2]);
+
+            var wi = new TfsWorkItem(args[0]);
+
+            var wiList = wi.GetWorkItemsFromId(args[1], args[2]);
+
+            var changeFileList = wi.GetChangeFileList(wiList);
+
+            Console.WriteLine("***** 修正ファイル一覧 開始 *****");
+            foreach (KeyValuePair<string, string> keyValuePair in changeFileList)
+            {
+                Console.WriteLine("File={0}, Changes={1}", keyValuePair.Key, keyValuePair.Value);
+            }
+            Console.WriteLine("***** 修正ファイル一覧 終了 *****");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListupEditFileFromWorkItem/TFSWorkItem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Collections.Generic;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace ListupEditFileFromWorkItem
+{
+    class TfsWorkItem
+    {
+        private readonly TfsTeamProjectCollection _tfs;
+        private readonly WorkItemStore _workItemStore;
+
+        public TfsWorkItem(string teamProjectCollectionUri)
+        {
+            _tfs = new TfsTeamProjectCollection(new Uri(teamProjectCollectionUri));
+            _workItemStore = _tfs.GetService<WorkItemStore>();
+        }
+
+        public WorkItemCollection GetWorkItemsFromId(string workItemName, string workItemId)
+        {
+            string queryString = 
+                string.Format("SELECT [タイトル] FROM WorkItems WHERE [作業項目の種類]='{0}' AND [ID]={1}", workItemName, workItemId);
+            return _workItemStore.Query(queryString);
+        }
+
+        public SortedDictionary<string, string> GetChangeFileList(WorkItemCollection wic)
+        {
+            var vcs = _tfs.GetService<VersionControlServer>();
+            var changeSourceFile = new SortedDictionary<string, string>();
+
+            foreach (WorkItem wi in wic)
+            {
+                for (var i = 0; i < wi.Links.Count; ++i)
+                {
+                    if (!wi.Links[i].ArtifactLinkType.Name.Equals("Fixed in Changeset")) continue;
+
+                    var uri = ((ExternalLink)wi.Links[i]).LinkedArtifactUri;
+                    int changeSetId;
+                    int.TryParse(uri.Substring(uri.LastIndexOf('/') + 1), out changeSetId);
+                    var changeSet = vcs.GetChangeset(changeSetId);
+                    foreach (var change in changeSet.Changes)
+                    {
+                        if (IsCollectChangeType(change.ChangeType) && IsCollectItemType(change.Item.ItemType))
+                        {
+                            if (!changeSourceFile.ContainsKey(change.Item.ServerItem))
+                            {
+                                changeSourceFile.Add(change.Item.ServerItem, change.ChangeType.ToString());
+                            }
+                            else
+                            {
+                                string value;
+                                changeSourceFile.TryGetValue(change.Item.ServerItem, out value);
+                                changeSourceFile.Remove(change.Item.ServerItem);
+                                changeSourceFile.Add(change.Item.ServerItem, value + ", " + change.ChangeType.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+            return changeSourceFile;
+        }
+
+        private bool IsCollectChangeType(ChangeType type)
+        {
+            if (type.HasFlag(ChangeType.Add) || type.HasFlag(ChangeType.Delete) || type.HasFlag(ChangeType.Edit) 
+            || type.HasFlag(ChangeType.Merge) || type.HasFlag(ChangeType.Rename) || type.HasFlag(ChangeType.Rollback))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsCollectItemType(ItemType type)
+        {
+            return type.HasFlag(ItemType.File);
+        }
+    }
+}
